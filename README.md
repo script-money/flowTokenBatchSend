b0deb8a170d3c767c3789c83bee37df2b67d676b
script-money <hello@script.money>
2021-08-19 16:16:42 +0800

    ✨ Support input amount in command args

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# Flow群发脚本
+# Flow 群发脚本
 
-从表格读取比例发放FlowToken。
+从表格读取比例发放 FlowToken。
 
 `pip install -r requirements.txt` 安装依赖
 
-.env.example重命名，然后填入自己的账户和私钥（私钥导出参考[Flow 主网命令行转账步骤](https://script.money/posts/027-flow_mainnet_cli_transaction/) ）
+.env.example 重命名去掉 example，然后填入自己的账户和私钥（私钥导出参考[Flow 主网命令行转账步骤](https://script.money/posts/027-flow_mainnet_cli_transaction/) ）
 
-修改send_multiple的参数，然后`python send_multiple.py`发起转账交易
+flow_test.csv.example 重命名去掉 example, 编辑要发送各地址的比例
+
+`python send_multiple.py [转账的总数]`发起转账交易
 
 > 余额查询 [https://flowscan.org/](https://flowscan.org/)
--- send_multiple.py
+++ send_multiple.py
@@ -2,22 +2,19 @@ import logging
 from flow_py_sdk import (
     cadence,
     flow_client,
-    SignAlgo,
-    HashAlgo,
-    AccountKey,
-    create_account_template,
     Tx,
     ProposalKey,
-    Script
 )
 import pandas as pd
 from config import Config
 import asyncio
 from pathlib import Path
 from datetime import date
+import argparse
 
 logger = logging.getLogger(__name__)
 ctx = Config()
+
 
 async def send_multiple(addresses_arg: cadence.Dictionary):
     async with flow_client(
@@ -27,53 +24,65 @@ async def send_multiple(addresses_arg: cadence.Dictionary):
         proposer = await client.get_account_at_latest_block(
             address=ctx.service_account_address.bytes
         )
-        tx = Tx(
-            code=open(Path(__file__).parent.joinpath(
-                "./transfer_many_accounts.cdc"))
-            .read(),
-            reference_block_id=block.id,
-            payer=ctx.service_account_address,
-            proposal_key=ProposalKey(
-                key_address=ctx.service_account_address,
-                key_id=ctx.service_account_key_id,
-                key_sequence_number=proposer.keys[
-                    ctx.service_account_key_id
-                ].sequence_number,
-            ),
-        ).add_authorizers(
-            ctx.service_account_address
-        ).add_arguments(
-            addresses_arg
-        ).with_gas_limit(
-            9999
-        ).with_envelope_signature(
-            ctx.service_account_address,
-            ctx.service_account_key_id,
-            ctx.service_account_signer,
+        tx = (
+            Tx(
+                code=open(
+                    Path(__file__).parent.joinpath("./transfer_many_accounts.cdc")
+                ).read(),
+                reference_block_id=block.id,
+                payer=ctx.service_account_address,
+                proposal_key=ProposalKey(
+                    key_address=ctx.service_account_address,
+                    key_id=ctx.service_account_key_id,
+                    key_sequence_number=proposer.keys[
+                        ctx.service_account_key_id
+                    ].sequence_number,
+                ),
+            )
+            .add_authorizers(ctx.service_account_address)
+            .add_arguments(addresses_arg)
+            .with_gas_limit(9999)
+            .with_envelope_signature(
+                ctx.service_account_address,
+                ctx.service_account_key_id,
+                ctx.service_account_signer,
+            )
         )
         await client.execute_transaction(tx)
 
-async def main(path:str, total:float):
+
+async def main(path: str, total: float):
     """
     path: csv路径
     total: 要分发的总数
     """
     df = pd.read_csv(path)
-    df['比例'] =  df['占比'] / df['占比'].sum()
+    df["比例"] = df["占比"] / df["占比"].sum()
     distribution = list(
         map(
-            lambda row:{'key':row[3],'value':round(row[4]*total,8)},
-            df.to_records()
+            lambda row: {"key": row[3], "value": round(row[4] * total, 8)},
+            df.to_records(),
         )
     )
-    pd.DataFrame.from_records(distribution).to_csv(f'record-{date.today()}.csv')
+    pd.DataFrame.from_records(distribution).to_csv(f"record-{date.today()}.csv")
     addresses_arg = cadence.Dictionary(
-        list(map(lambda i:cadence.KeyValuePair(
-                cadence.Address.from_hex(i['key']), 
-                cadence.UFix64(i['value']*1e8) # 100*1e-8 Flow
-            ) , distribution))
+        list(
+            map(
+                lambda i: cadence.KeyValuePair(
+                    cadence.Address.from_hex(i["key"]),
+                    cadence.UFix64(i["value"] * 1e8),  # 100*1e-8 Flow
+                ),
+                distribution,
+            )
+        )
     )
+    print(addresses_arg)
     await send_multiple(addresses_arg)
 
+
 if __name__ == "__main__":
-    asyncio.run(main('flow_test.csv',0.03))
+    parser = argparse.ArgumentParser(description="Send F to multiple accounts")
+    parser.add_argument("amount", type=float, help="flowToken amount (.8f)")
+    args = parser.parse_args()
+    send_amount = vars(args)["amount"]
+    asyncio.run(main("flow_test.csv", send_amount))
